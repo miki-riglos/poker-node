f65f70c9024a835f35b1fed87e6b1fc2995534f6
Miki Riglos <miki.riglos@gmail.com>
2011-10-20 22:33:49 +0000

    matcher toBeInstance added to jasmine

---
+++ readme.md
@@ -0,0 +1,19 @@
+#poker-node
+
+This is my pet project to develop a virtual Poker Texas Hold'em table while learning the following:
+
+Server
+------
+  * nodejs
+  * jasmine-node
+  * socket.io
+
+Client
+------
+  * socket.io.js / WebSockets
+  * jQuery for DOM manipulation
+  * A JS Framework to define: ko, backbone, etc for architecture
+
+IDE
+---
+  * cloud9
--- spec/cards.spec.js
+++ spec/cards.spec.js
@@ -1,56 +1,64 @@
 var Card = require("../lib/cards").Card,
     Deck = require("../lib/cards").Deck;
 
+// Adding matcher toBeInstaceOf
+jasmine.Matchers.prototype.toBeInstanceOf = function(pseudoClass){
+    return this.actual instanceof pseudoClass;
+};
+
 describe("Card class", function() {
 
     it("should create new instance", function() {
-        var cardAS = new Card("A", "S");
-        expect( cardAS ).toBeDefined();
+        var cardAceOfSpades = new Card("A", "S");
+        expect( cardAceOfSpades ).toBeInstanceOf( Card );
     });
 
     it("should have a toString method", function() {
-        var cardAS = new Card("A", "S");
-        expect( cardAS.toString() ).toEqual("Ace of Spades");
+        var cardAceOfSpades = new Card("A", "S");
+        expect( cardAceOfSpades.toString() ).toEqual("Ace of Spades");
     });
 
     it("should validate rank and suit", function() {
-        expect( function() { new Card("X", "S"); } ).toThrow(new Error("Invalid Rank/Suit combination"));
-        expect( function() { new Card("A", "X"); } ).toThrow("Invalid Rank/Suit combination");
-        expect( function() { new Card("X", "X"); } ).toThrow();
+        expect( function(){ new Card("X", "S"); } ).toThrow(new Error("Invalid Rank/Suit combination"));
+        expect( function(){ new Card("A", "X"); } ).toThrow("Invalid Rank/Suit combination");
+        expect( function(){ new Card("X", "X"); } ).toThrow();
     });
 
 });
 
 describe("Deck class", function() {
-    var deck, deckCards;
+    var deck;
     
     beforeEach(function() {
         deck = new Deck();
-        deckCards = deck.cards;
     });
     
     it("should create new instance", function() {
-        expect( deck ).toBeDefined();
+        expect( deck ).toBeInstanceOf( Deck );
     });
 
-    it("should have 52 cards", function() {
-        expect( deckCards.length ).toEqual(52);
+    it("should inherit from array", function() {
+        expect( deck ).toBeInstanceOf( Array );
+    });
+
+it("should have 52 cards", function() {
+        expect( deck.length ).toEqual(52);
     });
 
     it("should have a shuffle method", function() {
         deck.shuffle();
-        //console.log(deckCards);
-        expect( deckCards.length ).toEqual(52);
+        //console.log(deck);
+        expect( deck.length ).toEqual(52);
     });
 
     it("should have a deal method", function() {
-        var dealt;
+        var cardDealt;
         deck.shuffle();
-        dealt = deck.deal();
+        cardDealt = deck.deal();
         //console.log(dealt);
-        //console.log(deckCards);
-        expect( dealt ).toBeDefined();
-        expect( deckCards.length ).toEqual(51);
+        //console.log(deck);
+        expect( cardDealt ).toBeInstanceOf( Card );
+        expect( deck.length ).toEqual(51);
     });
 
 });
