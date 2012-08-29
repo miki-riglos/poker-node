58d18dd508defe1e5c880014de29648add53e643
Miki Riglos <miki.riglos@gmail.com>
2012-08-28 20:12:03 -0400

    Events in Tournament and Game

--- test/games.spec.js
+++ test/games.spec.js
@@ -1,10 +1,22 @@
 var Game = require("../lib/games").Game;
 
 describe("Game class", function() {
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
 
   it("should create new instance", function() {
-    var game = new Game();
     game.should.be.an.instanceOf( Game );
+  });
+
+  it("should have events", function(done) {
+    game.on("event", function() {
+      game.should.be.ok;
+      done();
+    });    
+    game.emit("event");
   });
 
 });
--- test/tournaments.spec.js
+++ test/tournaments.spec.js
@@ -11,6 +11,14 @@ describe("Tournament class", function() {
     tournament.should.be.an.instanceOf( Tournament );
   });
 
+  it("should have events", function(done) {
+    tournament.on("event", function() {
+      tournament.should.be.ok;
+      done();
+    });    
+    tournament.emit("event");
+  });
+  
   it("should accept options", function() {
     var tournamentWithOptions = new Tournament( {initialChips: 100} );
     tournamentWithOptions.options.initialChips.should.equal( 100 );
@@ -20,7 +28,7 @@ describe("Tournament class", function() {
     it("should allow players registration", function() {
       tournament.registerPlayer(1, "Sofia");
       tournament.registerPlayer(2, "Bianca");
-      Object.keys( tournament.registeredPlayers ).length.should.equal( 2 );
+      Object.keys( tournament.players ).length.should.equal( 2 );
     });  
 
     it("should validate position uniqueness", function() {
@@ -48,6 +56,74 @@ describe("Tournament class", function() {
       }).should.throwError();
     });  
     
+    it("should auto start when maximum number of players are registered", function() {
+      var i =0;
+      while (++i <= 10) {
+        tournament.registerPlayer(i, "Player" + i);
+      }
+      
+      (function() { 
+        tournament.registerPlayer(11, "Player 11");
+      }).should.throwError();
+    });  
+
   });
 
+  describe("Active Players", function() {
+    beforeEach(function() {
+      tournament.registerPlayer(1, "Sofia");
+      tournament.registerPlayer(2, "Bianca");
+    });
+
+    it("should return active players", function() {
+      Object.keys( tournament.getActivePlayers() ).length.should.equal( 2 );
+    });
+
+    it("should exclude players with no chips", function() {
+      tournament.players[2].chips = 0;
+      Object.keys( tournament.getActivePlayers() ).length.should.equal( 1 );
+    });
+  });
+  
+  describe("Game cycles", function() {
+    beforeEach(function() {
+      tournament.registerPlayer(1, "Sofia");
+      tournament.registerPlayer(2, "Bianca");
+    });
+
+    it("should emit start and end events", function(done) {
+      var counter = {
+        tournament: {start: 0, end: 0},
+        game      : {start: 0, end: 0}
+      };
+      
+      tournament.on("tournament-start", function() {
+        counter.tournament.start++;
+      });
+      tournament.on("game-start", function() {
+        counter.game.start++;
+        
+        tournament.players[2].chips -= 5000;
+        tournament.players[1].chips += 5000;
+        tournament.currentGame.end();
+      });
+      tournament.on("game-end", function() {
+        counter.game.end++;
+      });
+      tournament.on("tournament-end", function() {
+        counter.tournament.end++;
+        
+        counter.tournament.start.should.equal(1);
+        counter.game.start.should.equal(2);
+        counter.game.end.should.equal(2);
+        counter.tournament.end.should.equal(1);
+        
+        done();
+      });
+      
+      tournament.start();
+      
+    });
+
+  });
 });
