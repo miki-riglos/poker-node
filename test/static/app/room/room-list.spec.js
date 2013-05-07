48edf9e842ba9c13d5866eea4ea1eb5d51a92acb
Miki Riglos <miki.riglos@gmail.com>
2013-05-07 16:15:52 -0400

    test remove room-list vm

--- test/static/app/room/room-list.spec.js
+++ test/static/app/room/room-list.spec.js
@@ -24,6 +24,20 @@ define(['user/user', 'room/room-list'], function(User, RoomList) {
       roomList.add();
     });
 
+    it('should remove room', function(done) {
+      user.name('miki');
+      roomList.add();
+
+      roomList.allRooms().should.have.lengthOf(1);
+
+      roomList.allRooms.subscribe(function(newValue) {
+        roomList.allRooms().should.have.lengthOf(0);
+        done();
+      });
+
+      roomList.remove(roomList.allRooms()[0]);
+    });
+
   });
 
 });
